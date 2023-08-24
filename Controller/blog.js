54b4be9263b321fe7f99c8769b978669519fe4b5
Tarun Bylapudi <bylasoft@gmail.com>
2023-08-24 15:05:44 +0530

    created

---
+++ Controller/blog.js
@@ -0,0 +1,155 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+const Blog = require("../model/Blog");
+
+const produceMessage = require("../config/kafkaProducerConfig");
+
+exports.getBlogs = async (req, res, next) => {
+  let query;
+  if (req.query.category) {
+    query = { category: req.query.category };
+  }
+  if (req.query.category === "All") {
+    query = {};
+  }
+
+  if (req.query.toDate && req.query.fromDate) {
+    const { fromDate, toDate } = req.query;
+    query = {
+      ...query,
+      createdAt: {
+        $gte: new Date(fromDate),
+        $lte: new Date(toDate),
+      },
+    };
+  }
+  //console.log(query);
+  try {
+    const blogs = await Blog.find(query);
+
+    //console.log(blogs.length);
+
+    if (!blogs) {
+      await produceMessage("No blogs found!");
+      throw new ErrorResponse(`No blogs found!`, 404);
+    }
+
+    await produceMessage("all blogs fetched from DB");
+
+    res.status(200).json({ success: true, count: blogs.length, data: blogs });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getBlog = async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      await produceMessage(`Blog not found with id : ${req.params.id}`);
+      throw new ErrorResponse(`Blog not found with id : ${req.params.id}`, 404);
+    }
+    await produceMessage(`Blog details fetched from DB : ${req.params.id}`);
+    res.status(200).json({ success: true, data: blog });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateBlog = async (req, res, next) => {
+  const user = req.headers["x-current-user"];
+  console.log(req.headers);
+  console.log(user);
+  try {
+    let blog = await Blog.findById(req.params.id);
+
+    if (!blog) {
+      await produceMessage(`Blog not found with id : ${req.params.id}`);
+      throw new ErrorResponse(`Blog not found with id : ${req.params.id}`, 404);
+    }
+
+    //check if the user is the owner
+    if (blog.user.toString() !== user) {
+      await produceMessage(
+        `User with id ${user} is not authorized to update this blog`
+      );
+      throw new ErrorResponse(
+        `User with id ${user} is not authorized to update this blog`,
+        400
+      );
+    }
+
+    blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    await produceMessage("Blog details updated successfully!");
+    res.status(200).json({ success: true, data: blog });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getMyBlogs = async (req, res, next) => {
+  let query;
+  const user = req.headers["x-current-user"];
+
+  query = { ...req.query, user };
+
+  try {
+    const blogs = await Blog.find(query);
+
+    if (!blogs) {
+      await produceMessage(`No blogs found for user ${user}`);
+      throw new ErrorResponse(`No blogs found for user ${user}`, 404);
+    }
+    await produceMessage("Blogs associated with the user retrived from DB!");
+    res.status(200).json({ success: true, count: blogs.length, data: blogs });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.addBlog = async (req, res, next) => {
+  req.body.user = req.headers["x-current-user"];
+
+  console.log(req.headers["x-current-user"]);
+  try {
+    const blog = await Blog.create(req.body);
+    await produceMessage("New Blog is Created!");
+    res.status(200).json({ success: true, data: blog });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteBlog = async (req, res, next) => {
+  const user = req.headers["x-current-user"];
+
+  //console.log(user);
+  //console.log(req.params.id);
+  try {
+    const blog = await Blog.findById(req.params.id);
+    //console.log(blog);
+
+    if (!blog) {
+      await produceMessage(`Blog not found with id : ${req.params.id}`);
+      throw new ErrorResponse(`Blog not found with id : ${req.params.id}`, 404);
+    }
+    //check if the user is the owner
+    if (blog.user.toString() !== user) {
+      await produceMessage(
+        `User with id ${user} is not authorized to delete this blog`
+      );
+      throw new ErrorResponse(
+        `User with id ${user} is not authorized to delete this blog`,
+        400
+      );
+    }
+    blog.deleteOne();
+    await produceMessage("Blog is deleted!");
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const morgan = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const promClient = require("prom-client");
+
+const connectDB = require("./config/db");
+const blogRoutes = require("./routes/blog");
+const errorHandler = require("./middleware/errorHandler");
+
+const specs = require("./utils/swagger");
+
+//dotenv config
+dotenv.config({ path: "config/config.env" });
+
+//DB connection
+connectDB();
+
+const app = express();
+
+//api-docs
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+//body parser
+app.use(express.json());
+
+//logger
+app.use(
+  morgan("combined", {
+    skip: (req, res) => req.url === "/metrics?",
+  })
+);
+
+//sanitize data
+app.use(mongoSanitize());
+
+//set security headers
+app.use(helmet());
+
+//prevent xss attacks
+app.use(xss());
+
+//rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 500, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
+//prevent http param pollution
+app.use(hpp());
+
+//allow cors
+app.use(cors());
+
+//routes
+app.use("/api/v1/blogs", blogRoutes);
+
+//prometheus
+// Create a new Registry to store all metrics
+const registry = new promClient.Registry();
+
+// Set the default metrics
+promClient.collectDefaultMetrics({ register: registry });
+
+// Expose the metrics endpoint
+app.get("/metrics", async (req, res) => {
+  try {
+    res.set("Content-Type", promClient.register.contentType);
+    const metrics = await registry.metrics();
+    res.status(200).end(metrics);
+  } catch (error) {
+    res.status(500).end(error);
+  }
+});
+
+//error handler
+app.use(errorHandler);
+
+const port = process.env.PORT || 8001;
+
+const server = app.listen(
+  port,
+  console.log(`blog-service started running on port ${port}`.yellow.bold)
+);
+
+//handle unhandled promises
+process.on("unhandledRejection", (err, Promise) => {
+  console.log(`Error : ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
